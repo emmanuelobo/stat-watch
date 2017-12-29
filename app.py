da18e5412d4e777d338e49cb830757a9f654e380
= <=>
2017-12-28 23:47:28 -0500

    search functionality working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from nba_py.player import get_player
 from nba_py import player
 from flask_sqlalchemy import SQLAlchemy
@@ -29,6 +29,38 @@ def login():
 def register():
 	return render_template('signup.html')
 
+@app.route('/search', methods = ['POST', 'GET'])
+def search():
+	if request.method == 'POST':
+		first_name = request.form['first name'].strip()
+		last_name = request.form['last name'].strip()
+		try:
+			pid = get_player(first_name,last_name)
+			searched_player = player.PlayerSummary(pid)
+			print(searched_player.headline_stats()[0])
+			player_name = searched_player.headline_stats()[0]['PLAYER_NAME']
+			player_ppg = searched_player.headline_stats()[0]['PTS']
+			player_apg = searched_player.headline_stats()[0]['AST']
+			player_rpg = searched_player.headline_stats()[0]['REB']
+
+			data = {}
+			data['player_name'] = player_name
+			data['player_ppg'] = player_ppg
+			data['player_rpg'] = player_rpg
+			data['player_apg'] = player_apg
+
+		except StopIteration:
+			return 'Oops it appears that player doesn\'t exist. Please try again.'
+
+	return render_template('searched_player.html', **data)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+
+# TODO: Wireframing for pages
+# TODO: Create error page
+# TODO: Fix DB creation issue
+# TODO: Create user login/signup
+# TODO: Create add/remove player functionality
