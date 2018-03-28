d14a0b6ebd01420d8da0fd897278d511a8bdf9f1
= <emmaoobo@gmail.com>
2018-03-28 00:48:13 -0400

    testing configs

--- test_users.py
+++ test_users.py
@@ -7,6 +7,7 @@ from flask import url_for
 import tempfile
 
 from application import app, db
+from scripts.models import PlayerProfile
 
 TEST_DB = 'test.db'
 
@@ -32,12 +33,18 @@ class UserTests(unittest.TestCase):
 		db.drop_all()
 		self.app_context.pop()
 
-	def test_home_page(self):
-		response = self.client.get('/')
-		# import ipdb;ipdb.set_trace()
-		print('Test running...')
-		self.assertEqual(response.status_code, 200)
+	# def test_home_page(self):
+	# 	response = self.client.get('/')
+	# 	# import ipdb;ipdb.set_trace()
+	# 	print('Test running...')
+	# 	self.assertEqual(response.status_code, 200)
 
+	def test_players(self):
+		players = PlayerProfile.query.all()
+		for player in players:
+			print(player.full_name)
+
+		self.assertTrue(len(players) > 0)
 
 if __name__ == '__main__':
 	unittest.main()
