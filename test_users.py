c8c445bdf0dd8b48f871cada974b0c8c55168d1d
= <emmaoobo@gmail.com>
2018-03-28 22:41:42 -0400

    Test DB Config

--- test_users.py
+++ test_users.py
@@ -20,8 +20,8 @@ class UserTests(unittest.TestCase):
 	def setUp(self):
 		app.config['TESTING'] = True
 		app.config['DEBUG'] = True
-		# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(config('BASE_DIR'), TEST_DB)
-		app.config['DATABASE'] = tempfile.mkstemp()
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(config('BASE_DIR'), TEST_DB)
+		# app.config['DATABASE'] = tempfile.mkstemp()
 		self.app = app
 		self.app_context = self.app.app_context()
 		self.app_context.push()
@@ -29,22 +29,21 @@ class UserTests(unittest.TestCase):
 		db.create_all()
 
 	def tearDown(self):
-		db.session.remove()
-		db.drop_all()
+		# db.session.remove()
+		# db.drop_all()
 		self.app_context.pop()
 
-	# def test_home_page(self):
-	# 	response = self.client.get('/')
-	# 	# import ipdb;ipdb.set_trace()
-	# 	print('Test running...')
-	# 	self.assertEqual(response.status_code, 200)
+	def test_home_page(self):
+		response = self.client.get(url_for('home'))
+		# import ipdb;ipdb.set_trace()
+		print('Test running...')
+		self.assertEqual(response.status_code, 200)
 
-	def test_players(self):
-		players = PlayerProfile.query.all()
-		for player in players:
-			print(player.full_name)
-
-		self.assertTrue(len(players) > 0)
+	# def test_players(self):
+	# 	players = PlayerProfile.query.all()
+	# 	for player in players:
+	# 		print(player.full_name)
+	# 	self.assertTrue(len(players) > 0)
 
 if __name__ == '__main__':
 	unittest.main()
