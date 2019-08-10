94897e8f2410441593fbdcbf5c8e76417f9fb7ae
Emmanuel Obogbaimhe <emmaoobo@gmail.com>
2019-08-09 21:32:23 -0400

    search player api

--- api_server.py
+++ api_server.py
@@ -3,11 +3,16 @@ import json
 from flask_restful import Resource, Api, reqparse
 from flask import Flask
 from nba_py import player
+from nba_py.player import get_player
 
 from application import app, db
 from scripts.playerutil import PlayerUtility
 
 api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('firstname')
+parser.add_argument('lastname')
 
 
 class PlayerProfileResource(Resource):
@@ -24,14 +29,38 @@ class PlayerProfileResource(Resource):
 		pass
 
 
+class SearchedPlayer(Resource):
+	def get(self):
+		args = parser.parse_args()
+		first_name = args['firstname']
+		last_name = args['lastname']
+		try:
+			pid = get_player(first_name, last_name)
+			current_player = player.PlayerSummary(pid)
+			stats = current_player.headline_stats()[0]
+			info = current_player.info()[0]
+			profile_pic = PlayerUtility.get_profile_pic(info)
+		except StopIteration:
+			try:
+				pid = get_player(last_name, first_name)
+				current_player = player.PlayerSummary(pid)
+				stats = current_player.headline_stats()[0]
+				info = current_player.info()[0]
+				profile_pic = PlayerUtility.get_profile_pic(info)
+
+			except StopIteration:
+				return {'error_message': 'Oops it appears that player doesn\'t exist. Please try again.'}
+		return {'player': info,
+				'stats': stats,
+				'profile_pic': profile_pic}
+
+
 class PlayerLastGames(Resource):
 	def get(self):
 		pass
 
 
-
-
 if __name__ == '__main__':
 	api.add_resource(PlayerProfileResource, '/players/<int:player_id>/profile')
-
+	api.add_resource(SearchedPlayer, '/players/search')
 	app.run(host='0.0.0.0', port=8080, debug=True)
